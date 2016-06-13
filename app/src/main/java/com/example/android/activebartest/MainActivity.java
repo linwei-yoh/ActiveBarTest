b9fd54fe4b6df288ebc1f550c1608f8109cd5cef
linwei-yoh <395373035@qq.com>
2016-06-13 17:14:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/activebartest/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.activebartest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
+        toolbar.setTitle("Title Test");
+        setSupportActionBar(toolbar);
+}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE,0,1,"menu1");
+        menu.add(Menu.NONE,0,3,"menu2");
+        menu.add(Menu.NONE,0,2,"menu3");
+        return  true;
+    }
+}
